1d11efef05c2c39b337c8193de122bcbce148ca6
Sidharth Menon <sidharth@startupmission.in>
2024-06-20 11:36:35 +0000

    changes

--- src/components/BackTop.svelte
+++ src/components/BackTop.svelte
@@ -32,7 +32,7 @@
 
 <svelte:window bind:scrollY={y}/>
 
-<div on:click={handleNav} data-target="container" data-offset="0" class="{scrolled ? 'fixed' : 'hidden'} bottom-5 right-5 cursor-pointer bg-white rounded-full w-14 h-14 flex items-center justify-center z-50 text-huddle">
+<div on:click={handleNav} data-target="home" data-offset="0" class="{scrolled ? 'fixed' : 'hidden'} bottom-5 right-5 cursor-pointer bg-white rounded-full w-14 h-14 flex items-center justify-center z-50 text-huddle">
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
   </svg>  
