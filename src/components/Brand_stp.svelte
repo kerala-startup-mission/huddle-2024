7d83f97cbb0cb864abf4c12dd49ef84a143c8b19
ANUSREEPURUSHOTHAMANstartupmission <anusree@startupmission.in>
2024-10-25 16:38:53 +0530

    commit

--- src/components/Brand_stp.svelte
+++ src/components/Brand_stp.svelte
@@ -113,8 +113,8 @@
          
                     {/each}
       
-                    <button id="closeButton"  on:click={close_popup} class="absolute m-2 top-3 right-3 rounded-full bg-slate-900 text-white closePopup " >
-                      <svg class="h-5 w-5" fill="white" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <button id="closeButton"  on:click={close_popup} class="absolute m-2 top-1  right-0 rounded-full bg-slate-900 text-white closePopup " >
+                      <svg class="h-4 w-4" fill="white" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                       </svg>
                     </button>
