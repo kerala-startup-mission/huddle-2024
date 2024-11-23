32a6d28c111e6a12373c642ebccc1f5085089bd2
ANUSREEPURUSHOTHAMANstartupmission <anusree@startupmission.in>
2024-11-23 22:16:14 +0530

    first commit

--- src/components/Productexpo.svelte
+++ src/components/Productexpo.svelte
@@ -107,7 +107,7 @@
   
   <div class="sm:px-6 sm:m-0 m-4">
     <!-- Filter Icon -->
-    <div class=" mb-4  items-end justify-end flex flex-col">
+    <div class=" mb-6  items-end justify-end flex flex-col">
       <button on:click={() => (showSectors = !showSectors)} class="p-2 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
@@ -119,7 +119,7 @@
           {#each sectors as sector}
             <div
               on:click={() => selectSector(sector)}
-              class="px-4 py-2 text-black cursor-pointer text-start hover:bg-gray-100"
+              class="px-4 py-2 text-black cursor-pointer text-start md:text-base text-sm hover:bg-gray-100"
             >
               {sector}
             </div>
